e940ed024d215e98f08a2818d12a1c7b9f1ed4a9
Luis Regueira <luis.regueira@schibsted.com>
2023-08-18 15:31:57 +0200

    Trying some stuff...

--- Package.swift
+++ Package.swift
@@ -4,21 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "Warp-ios",
+    name: "warp-ios",
     platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Warp-ios",
-            targets: ["Warp-ios"]),
+            name: "warp-ios",
+            targets: ["warp-ios"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Warp-ios"),
+            name: "warp-ios",
+            path: "Sources"),
         .testTarget(
             name: "Warp-iosTests",
-            dependencies: ["Warp-ios"]),
+            dependencies: ["warp-ios"]),
     ]
 )
