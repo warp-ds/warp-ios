8a435532e27897a6eb64b31395951b737db1552e
Sajad Vishkai <sajad.vishkai@schibsted.com>
2023-09-06 10:30:07 +0200

    Update package file, update package name.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Warp-ios",
+    name: "warp-ios",
     platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
