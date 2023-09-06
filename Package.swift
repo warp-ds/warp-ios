1481cf2cad5c6547cecdc2a2c12b75bec27d8ea6
Sajad Vishkai <sajad.vishkai@schibsted.com>
2023-09-06 10:30:44 +0200

    Update package file, update target name and source path.

--- Package.swift
+++ Package.swift
@@ -16,7 +16,9 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Warp-ios"),
+            name: "warp-ios",
+            path: "Sources"
+        ),
         .testTarget(
             name: "Warp-iosTests",
             dependencies: ["Warp-ios"]),
