976591ab2fe0b684c321a5538c6cf1bf259968c8
Sajad Vishkai <sajad.vishkai@schibsted.com>
2023-09-22 13:29:26 +0200

    Rework `UtilityTertiaryButton`.

--- Sources/Button/UtilityTertiaryButton.swift
+++ Sources/Button/UtilityTertiaryButton.swift
@@ -1,32 +1,72 @@
+import Foundation
 import SwiftUI
 
-public struct UtilityTertiaryButton: View {
-    let title: String
-    let icon: String
-    let size: WarpButtonSize
-    let disabled: Bool
-    let fullWidth: Bool
-    let colorProvider = Config.colorProvider
+extension WarpButton {
+    /// <#Description#>
+    static func createUtilityTertiary(
+        title: String,
+        icon: String? = nil,
+        action: @escaping () -> Void,
+        size: WarpButtonSize = .big,
+        isEnabled: Bool = false,
+        fullWidth: Bool = false
+    ) -> WarpButton {
+        WarpButton(
+            title: title,
+            icon: icon,
+            action: action,
+            type: .utilityTertiary,
+            size: size,
+            disbled: isEnabled,
+            fullWidth: fullWidth
+        )
+    }
+}
 
-    public init(title: String,
-                icon: String = "",
-                size: WarpButtonSize = .big,
-                disbled: Bool = false,
-                fullWidth: Bool = false) {
+public struct UtilityTertiaryButton: View {
+    /// <#Description#>
+    let title: String
+    
+    /// <#Description#>
+    let icon: String?
+
+    /// <#Description#>
+    let action: () -> Void
+
+    /// <#Description#>
+    let size: WarpButtonSize
+
+    /// <#Description#>
+    let disabled: Bool
+
+    /// <#Description#>
+    let fullWidth: Bool
+
+    public init(
+        title: String,
+        icon: String? = nil,
+        action: @escaping () -> Void,
+        size: WarpButtonSize = .big,
+        disbled: Bool = false,
+        fullWidth: Bool = false
+    ) {
         self.title = title
         self.icon = icon
+        self.action = action
         self.size = size
         self.disabled = disbled
         self.fullWidth = fullWidth
     }
-    
+
     public var body: some View {
-        WarpButton(title: title,
-                   icon: icon,
-                   type: .utilityTertiary,
-                   size: size,
-                   disbled: disabled,
-                   fullWidth: fullWidth)
+        WarpButton.createUtilityTertiary(
+            title: title,
+            icon: icon,
+            action: action,
+            size: size,
+            isEnabled: disabled,
+            fullWidth: fullWidth
+        )
     }
 }
 
