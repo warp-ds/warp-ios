e3ef7fb563731043bb12473042463e12b5da394e
Sajad Vishkai <sajad.vishkai@schibsted.com>
2023-09-25 12:15:42 +0200

    Remove `PrimaryButton` in order to redirect use-case to factory methods.

--- Sources/Button/PrimaryButton.swift
+++ Sources/Button/PrimaryButton.swift
@@ -43,53 +43,6 @@ extension Warp.Button {
     }
 }
 
-public struct PrimaryButton: View {
-    /// <#Description#>
-    let title: String
-
-    /// <#Description#>
-    let icon: String?
-
-    /// <#Description#>
-    let action: () -> Void
-
-    /// <#Description#>
-    let size: Warp.ButtonSize
-
-    /// <#Description#>
-    let isEnabled: Bool
-
-    /// <#Description#>
-    let fullWidth: Bool
-
-    public init(
-        title: String,
-        icon: String?,
-        action: @escaping () -> Void,
-        size: Warp.ButtonSize,
-        isEnabled: Bool,
-        fullWidth: Bool
-    ) {
-        self.title = title
-        self.icon = icon
-        self.action = action
-        self.size = size
-        self.isEnabled = isEnabled
-        self.fullWidth = fullWidth
-    }
-
-    public var body: some View {
-        Warp.Button.createPrimary(
-            title: title,
-            icon: icon,
-            action: action,
-            size: size,
-            isEnabled: isEnabled,
-            fullWidth: fullWidth
-        )
-    }
-}
-
 private struct PrimaryButtonPreview: PreviewProvider {
     static var previews: some View {
         VStack(spacing: 8) {
