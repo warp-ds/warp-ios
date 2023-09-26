bf732810dcfcd384d0eafb87ad2affddc96003d1
Sajad Vishkai <sajad.vishkai@schibsted.com>
2023-09-26 10:41:30 +0200

    Introduce `Warp.Button.Icon`.

--- Sources/Button/WarpButton.swift
+++ Sources/Button/WarpButton.swift
@@ -104,6 +104,28 @@ extension Warp {
     }
 }
 
+extension Warp.Button {
+    /// Wrapper around image name for providing more context related to button icon.
+    public struct Icon: ExpressibleByStringLiteral {
+        /// Image name.
+        public let name: String
+
+        /// Image description,
+        /// will be used to describe image in accessibility label.
+        public let description: String?
+
+        public init(name: String, description: String? = nil) {
+            self.name = name
+            self.description = description
+        }
+
+        public init(stringLiteral value: String) {
+            self.name = value
+            description = nil
+        }
+    }
+}
+
 /// Helper constructors introduced to extend image capabilities.
 extension Warp.Button {
     public init(
