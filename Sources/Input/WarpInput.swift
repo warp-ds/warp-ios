350e4c9ee3e66bf0ecceb1c33b9a3ffc7d12d96b
Sajad Vishkai <sajad.vishkai@schibsted.com>
2023-10-02 12:34:04 +0200

    Rework input.

--- Sources/Input/WarpInput.swift
+++ Sources/Input/WarpInput.swift
@@ -53,6 +53,247 @@ extension Warp {
 
     }
 }
+
+extension Warp {
+    public struct Input: View {
+        /// <#Description#>
+        public static let inputDefaultInactiveState = Warp.InputState.normal
+
+        private let title: String
+        
+        /// <#Description#>
+        public var text: Binding<String>
+        
+//        private var _state: Binding<Warp.InputState>
+        /// <#Description#>
+        @Binding private var state: Warp.InputState
+
+        private let colorProvider = Config.colorProvider
+
+        public init(
+            title: String,
+            text: Binding<String>,
+            state: Binding<Warp.InputState>
+        ) {
+            self.title = title
+            self.text = text
+            self._state = state
+        }
+
+        init(
+            title: String,
+            text: String = "",
+            state: Warp.InputState = inputDefaultInactiveState
+        ) {
+            self.title = title
+            self.text = .constant(text)
+            self._state = .constant(state)
+
+//            self._state = .init(
+//                get: {
+//                    state
+//                }, set: { newState in
+//                    _state.wrappedValue = newState
+//                }
+//            )
+        }
+
+        private var inputBorderColor: Color {
+            state.inputBorderColor
+        }
+
+        private var backgroundColor: Color {
+            state.backgroundColor
+        }
+
+        private var helpTextForegroundColor: Color {
+            state.helpTextForegroundColor
+        }
+
+        private var inputBorderWidth: CGFloat {
+            state.inputBorderWidth
+        }
+
+        private var shouldBeDisabled: Bool {
+            state.shouldBeDisabled
+        }
+
+        private var horizontalPadding: CGFloat {
+            state.horizontalPadding
+        }
+
+        public var body: some View {
+            VStack(alignment: .leading) {
+                topView
+
+                textFieldView
+
+                helperTextView
+            }
+            .padding()
+        }
+
+        // MARK: - TopView
+
+        private var topView: some View {
+            HStack {
+                titleView
+
+                optionalLabelView
+
+                rightImageView
+            }
+        }
+
+        private var titleView: some View {
+            Text(title)
+                .font(.footnote)
+                .foregroundColor(colorProvider.inputTextFilled)
+        }
+
+        private var optionalLabelView: some View {
+            Text("Optional")
+                .font(.caption)
+                .fontWeight(.thin)
+                .foregroundColor(FinnColors.gray500)
+        }
+
+        private var rightImageView: some View {
+            Image(systemName: "exclamationmark.circle")
+                .foregroundColor(FinnColors.gray300)
+        }
+
+        // MARK: TopView -
+
+        private var textFieldView: some View {
+            TextField(
+                "Hint",
+                text: text,
+                onEditingChanged: { startedEditing in
+                    withAnimation {
+                        if startedEditing {
+                            _state.wrappedValue = .active
+                        } else {
+                            _state.wrappedValue = .normal
+                        }
+                    }
+                }
+            )
+            .font(.callout)
+            .disabled(shouldBeDisabled)
+            .padding(.vertical)
+            .padding(.horizontal, horizontalPadding)
+            .background(backgroundColor)
+            .overlay(overlayView)
+            .cornerRadius(4)
+        }
+
+        private var overlayView: some View {
+            RoundedRectangle(cornerRadius: 4)
+                .stroke(inputBorderColor, lineWidth: inputBorderWidth)
+        }
+
+        private var helperTextView: some View {
+            Text(_state.wrappedValue == .error ? "Error text" : "Help text")
+                .foregroundColor(helpTextForegroundColor)
+                .font(.caption)
+                .fontWeight(.thin)
+        }
+    }
+}
+
+extension Warp {
+    /// <#Description#>
+    public enum InputState {
+        case normal
+        case active
+        case disabled
+        case error
+        case readOnly
+    }
+}
+
+extension Warp.InputState {
+    /// <#Description#>
+    fileprivate var inputBorderColor: Color {
+        let colorProvider = Config.colorProvider
+
+        switch self {
+            case .normal:
+                return colorProvider.inputBorder
+
+            case .active:
+                return colorProvider.inputBorderActive
+
+            case .disabled:
+                return colorProvider.inputBorderDisabled
+
+            case .error:
+                return colorProvider.inputBorderNegative
+
+            case .readOnly:
+                return colorProvider.inputBorder
+        }
+    }
+
+    private var isDisabled: Bool {
+        return self == .disabled
+    }
+
+    /// <#Description#>
+    fileprivate var backgroundColor: Color {
+        let colorProvider = Config.colorProvider
+
+        if self == .disabled {
+            return colorProvider.inputBackgroundDisabled
+        }
+
+        return colorProvider.inputBackground
+    }
+
+    /// <#Description#>
+    fileprivate var helpTextForegroundColor: Color {
+        let colorProvider = Config.colorProvider
+
+        if self == .error {
+            return colorProvider.inputTextNegative
+        }
+
+        return colorProvider.inputTextHint
+    }
+
+    /// <#Description#>
+    fileprivate var inputBorderWidth: CGFloat {
+        switch self {
+            case .normal:
+                return 2
+
+            case .active:
+                return 4
+
+            case .disabled:
+                return 2
+
+            case .error:
+                return 2
+
+            case .readOnly:
+                return 0
+        }
+    }
+
+    fileprivate var shouldBeDisabled: Bool {
+        let isDisabled = self == .disabled
+        lazy var isReadOnly = self == .readOnly
+
+        return isDisabled || isReadOnly
+    }
+
+    fileprivate var horizontalPadding: CGFloat {
+        self == .readOnly ? 0 : 8
+    }
+}
+
 public struct WarpInput: View {
     let title: String
     @State var text: String = ""
