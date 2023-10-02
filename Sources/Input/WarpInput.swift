7e03812efcdfc4a756de0beacc296af5fed56603
Sajad Vishkai <sajad.vishkai@schibsted.com>
2023-10-02 12:33:46 +0200

    Add `InputConfig`.

--- Sources/Input/WarpInput.swift
+++ Sources/Input/WarpInput.swift
@@ -1,5 +1,58 @@
 import SwiftUI
 
+extension Warp {
+    /// <#Description#>
+    public struct InputConfig {
+        /// <#Description#>
+        public let placeholder: String
+
+        /// <#Description#>
+        public let title: String?
+
+        /// <#Description#>
+        public let additionalInformation: String?
+
+        /// <#Description#>
+        public let infoToolTip: Image?
+
+        /// <#Description#>
+        public let iconLeft: Image?
+
+        /// <#Description#>
+        public let iconRight: Image?
+
+        /// <#Description#>
+        public let prefix: String?
+
+        /// <#Description#>
+        public let suffix: String?
+
+        public let isAnimated: Bool
+
+        init(
+            placeholder: String,
+            title: String? = nil,
+            additionalInformation: String? = nil,
+            infoToolTip: Image? = nil,
+            iconLeft: Image? = nil,
+            iconRight: Image? = nil,
+            prefix: String? = nil,
+            suffix: String? = nil,
+            isAnimated: Bool = false
+        ) {
+            self.placeholder = placeholder
+            self.title = title
+            self.additionalInformation = additionalInformation
+            self.infoToolTip = infoToolTip
+            self.iconLeft = iconLeft
+            self.iconRight = iconRight
+            self.prefix = prefix
+            self.suffix = suffix
+            self.isAnimated = isAnimated
+        }
+
+    }
+}
 public struct WarpInput: View {
     let title: String
     @State var text: String = ""
