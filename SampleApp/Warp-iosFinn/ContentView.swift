180c7fa1a1002074f70d8f7f089c3b54538c3924
Sajad Vishkai <sajad.vishkai@schibsted.com>
2023-10-10 11:22:46 +0200

    Rework application name.

--- SampleApp/Warp-iosFinn/ContentView.swift
+++ SampleApp/Warp-iosFinn/ContentView.swift
@@ -22,11 +22,19 @@ struct ContentView: View {
                 }
                 .padding()
             }
-            .navigationTitle( Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as! String)
+            .navigationTitle(Bundle.main.applicationName)
         }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+extension Bundle {
+    // Name of the app - title under the icon.
+    fileprivate var applicationName: String {
+        return object(forInfoDictionaryKey: "CFBundleDisplayName") as? String ??
+        object(forInfoDictionaryKey: "CFBundleName") as? String ?? ""
+    }
 }
