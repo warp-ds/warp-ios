9a2dc57814919a01d641fe215e6dab9007ae4cb6
Sajad Vishkai <sajad.vishkai@schibsted.com>
2024-01-25 18:45:14 +0100

    Introduce `ButtonsView`.

--- Sources/Alert/Alert.swift
+++ Sources/Alert/Alert.swift
@@ -1,5 +1,93 @@
 import Foundation
 import SwiftUI
+private struct ButtonsView: View, Hashable {
+    let primaryButtonProvider: Warp.Alert.ButtonConstructor?
+
+    let secondaryButtonProvider: Warp.Alert.ButtonConstructor?
+
+    let colorProvider: ColorProvider
+
+    static func == (lhs: ButtonsView, rhs: ButtonsView) -> Bool {
+        let primaryComparison: Bool
+
+        switch (lhs.primaryButtonProvider, rhs.primaryButtonProvider) {
+            case let (.some(lhsProvider), .some(rhsProvider)):
+                primaryComparison = lhsProvider.title == rhsProvider.title
+
+            case (nil, nil):
+                primaryComparison = true
+
+            case (.some, nil):
+                primaryComparison = false
+
+            case (nil, .some):
+                primaryComparison = false
+        }
+
+        let secondaryComparison: Bool
+
+        switch (lhs.secondaryButtonProvider, rhs.secondaryButtonProvider) {
+            case let (.some(lhsProvider), .some(rhsProvider)):
+                secondaryComparison = lhsProvider.title == rhsProvider.title
+
+            case (nil, nil):
+                secondaryComparison = true
+
+            case (.some, nil):
+                secondaryComparison = false
+
+            case (nil, .some):
+                secondaryComparison = false
+        }
+
+        return primaryComparison && secondaryComparison
+    }
+
+    func hash(into hasher: inout Hasher) {
+        if let primaryButtonProvider {
+            hasher.combine(primaryButtonProvider.title)
+        }
+
+        if let secondaryButtonProvider {
+            hasher.combine(secondaryButtonProvider.title)
+        }
+    }
+
+    init?(
+        primaryButtonProvider: Warp.Alert.ButtonConstructor?,
+        secondaryButtonProvider: Warp.Alert.ButtonConstructor?,
+        colorProvider: ColorProvider
+    ) {
+        if primaryButtonProvider == nil, secondaryButtonProvider == nil {
+            return nil
+        }
+
+        self.primaryButtonProvider = primaryButtonProvider
+        self.secondaryButtonProvider = secondaryButtonProvider
+        self.colorProvider = colorProvider
+    }
+
+    var body: some View {
+        HStack {
+            if let primaryButtonProvider {
+                Warp.Button.createSecondary(
+                    title: primaryButtonProvider.title,
+                    action: primaryButtonProvider.action
+                )
+            }
+
+            if let secondaryButtonProvider {
+                Warp.Button.createTertiary(
+                    title: secondaryButtonProvider.title,
+                    action: secondaryButtonProvider.action
+                )
+            }
+
+            Spacer()
+        }
+    }
+}
+
 private struct UnderlinedLinkModifier: ViewModifier {
     private var linkColor: Color {
         Color.blue
