ee48a1f7b958c8f285a4bcd5c09743b2365c0e11
Sajad Vishkai <sajad.vishkai@schibsted.com>
2024-01-25 18:41:12 +0100

    Introduce `Warp.AlertStyle`.

--- Sources/Alert/AlertStyle.swift
+++ Sources/Alert/AlertStyle.swift
@@ -1,2 +1,18 @@
 import Foundation
 
+extension Warp {
+    /// <#Description#>
+    public enum AlertStyle: Equatable {
+        /// <#Description#>
+        case info
+
+        /// <#Description#>
+        case warning
+
+        /// <#Description#>
+        case critical
+
+        /// <#Description#>
+        case success
+    }
+}
