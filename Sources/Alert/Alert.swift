d740fd0561a6e60fc394c5f2cf152d41ce9e5ec4
Sajad Vishkai <sajad.vishkai@schibsted.com>
2024-01-25 18:43:49 +0100

    Introduce `getBackgroundColor` method.

--- Sources/Alert/Alert.swift
+++ Sources/Alert/Alert.swift
@@ -1,4 +1,19 @@
 import Foundation
 import SwiftUI
 extension Warp.AlertStyle {
+    fileprivate func getBackgroundColor(from colorProvider: ColorProvider) -> Color {
+        switch self {
+            case .info:
+                return FinnColors.aqua50
+
+            case .warning:
+                return FinnColors.yellow50
+
+            case .critical:
+                return FinnColors.red50
+
+            case .success:
+                return FinnColors.green50
+        }
+    }
 }
