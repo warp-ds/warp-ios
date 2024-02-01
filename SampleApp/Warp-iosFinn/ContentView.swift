b9a9c79e3d618507726c268ddc7722ee255d7395
Mohsen <mohsen.hosseinpour@schibsted.com>
2024-02-01 17:06:06 +0100

    Add new version

--- SampleApp/Warp-iosFinn/ContentView.swift
+++ SampleApp/Warp-iosFinn/ContentView.swift
@@ -69,6 +69,9 @@ struct ContentView: View {
                         Text("WarpToast")
                             .padding()
                     }
+                    Divider()
+                    Text("\(Bundle.main.releaseVersionNumber) (\(Bundle.main.buildVersionNumber))")
+                        .font(.caption2)
                 }
                 .padding()
             }
@@ -86,5 +89,11 @@ extension Bundle {
     fileprivate var applicationName: String {
         return object(forInfoDictionaryKey: "CFBundleDisplayName") as? String ??
         object(forInfoDictionaryKey: "CFBundleName") as? String ?? ""
+    }
+    fileprivate var releaseVersionNumber: String {
+        return infoDictionary?["CFBundleShortVersionString"] as? String ?? ""
+    }
+    fileprivate var buildVersionNumber: String {
+        return infoDictionary?["CFBundleVersion"] as? String ?? ""
     }
 }
