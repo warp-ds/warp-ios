8dc516d7f5c79f29f733f31dd79828b273ecb523
Sajad Vishkai <144221668+vishkaischibsted@users.noreply.github.com>
2024-02-09 13:23:29 +0100

    Introduce `BoxStyle`.

---
+++ Sources/Box/BoxStyle.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+extension Warp {
+    /// Style for `Box` view.
+    public enum BoxStyle: Equatable {
+        /// Used to group information that separates itself from other elements in a page layout.
+        /// Use this box to show generic information that isn’t necessarily directly related to the main focus content on your page.
+        case neutral
+
+        /// Used to highlight content on a page. It differs from an “Info alert”, 
+        /// which is used in a more contextual manner, where the information is presented to the user as a prompt to take action.
+        /// Use this box to show information that is related, or relevant to the main focus content on your page.
+        case info
+
+        /// Can be used on coloured backgrounds, when a neutral or info box doesn’t work.
+        /// It can also be used as an alternative way to group information on a page that might need a variation of grouped information.
+        /// Use this box when showing generic information on coloured or low contrast backgrounds.
+        case bordered
+    }
+}
