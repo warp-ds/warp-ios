df044afe785d7414407a885af65d0772e51eb613
Sajad Vishkai <144221668+vishkaischibsted@users.noreply.github.com>
2024-02-12 18:05:07 +0100

    Introduce `createAlertView ` method.

--- SampleApp/AlertView.swift
+++ SampleApp/AlertView.swift
@@ -20,21 +20,64 @@ struct AlertView: View {
                     link: nil,
                     primaryButton: nil,
                     secondaryButton: nil
+    private func createAlertView(
+        basedOn state: (hasLink: Bool, hasPrimaryButton: Bool, hasSecondaryButton: Bool)
+    ) -> some View {
+        let linkProvider: Warp.Alert.ButtonConstructor? = {
+            if state.hasLink {
+                return (
+                    title: "Visit this link for more information",
+                    action: {
+                        UIApplication.shared.open(URL(string: "https://github.com/warp-ds/warp-ios")!)
+                    }
+                )
+            }
+
+            return nil
+        }()
+
+        let primaryButtonProvider: Warp.Alert.ButtonConstructor? = {
+            if state.hasPrimaryButton {
+                return (
+                    title: "Click me!",
+                    action: {
+                        // no-op
+                    }
+                )
+            }
+
+            return nil
+        }()
+
+        let secondaryButtonProvider: Warp.Alert.ButtonConstructor? = {
+            if state.hasSecondaryButton {
+                return (
+                    title: "Click me!",
+                    action: {
+                        // no-op
+                    }
                 )
                 .padding()
             }
 
-            ForEach(Warp.AlertStyle.allCases, id: \.self) { style in
-                Warp.Alert(
-                    style: style,
-                    title: "Title",
-                    subtitle: "Use this variant to call extra attention to useful, contextual information.",
-                    link: (title: "Link to more information", action: {}),
-                    primaryButton: (title: "Button", action: {}),
-                    secondaryButton: (title: "Button", action: {})
-                )
-                .padding()
-            }
+            return nil
+        }()
+
+        let style = style
+        let title = alertTitle
+        let subtitle = alertSubtitle
+
+        return Warp.Alert(
+            style: style,
+            title: title,
+            subtitle: subtitle,
+            link: linkProvider,
+            primaryButton: primaryButtonProvider,
+            secondaryButton: secondaryButtonProvider,
+            colorProvider: Warp.Config.colorProvider
+        )
+    }
+
     private func createToggleLabelView(hasValue: Bool, tag: String) -> some View {
         HStack {
             let prependStaticText = !hasValue ? "Add": "Remove"
