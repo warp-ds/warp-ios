c84b190f7e7a0e07e6ac5872ea9969dec0731575
Sajad Vishkai <144221668+vishkaischibsted@users.noreply.github.com>
2024-03-05 13:55:55 +0100

    Revisit `HelperInformationView`, and adopt new logics.

--- Sources/TextField/WarpInput/HelperInformationView.swift
+++ Sources/TextField/WarpInput/HelperInformationView.swift
@@ -6,34 +6,27 @@ struct HelperInformationView: View, Hashable {
     /// State of TextField.
     let textFieldState: Warp.TextFieldState
 
-    /// Information state, with possible associated message.
-    let informationState: Warp.TextField.InformationState
-
     /// Object responsible for providing colors.
     let colorProvider: ColorProvider
 
     static func == (lhs: HelperInformationView, rhs: HelperInformationView) -> Bool {
-        lhs.textFieldState == rhs.textFieldState &&
-        lhs.informationState == rhs.informationState
+        lhs.textFieldState == rhs.textFieldState
     }
 
     public func hash(into hasher: inout Hasher) {
         hasher.combine(textFieldState)
-        hasher.combine(informationState)
     }
 
     init(
         textFieldState: Warp.TextFieldState,
-        informationState: Warp.TextField.InformationState,
         colorProvider: ColorProvider
     ) {
         self.textFieldState = textFieldState
-        self.informationState = informationState
         self.colorProvider = colorProvider
     }
 
     private var foregroundColor: Color {
-        if textFieldState == .error {
+        if textFieldState.hasError {
             return colorProvider.inputTextNegative
         }
 
@@ -41,7 +34,7 @@ struct HelperInformationView: View, Hashable {
     }
 
     var body: some View {
-        if let helperText = informationState.helperText {
+        if let helperText = textFieldState.helperText {
             Text(helperText)
                 .foregroundColor(foregroundColor)
                 .font(.caption)
