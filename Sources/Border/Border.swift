6a6c5bdba5a39846ca493a705e34de71247d6ca3
Brian Dinsen <brian.dinsen@schibsted.com>
2024-07-01 11:29:21 +0200

    Remove commented out code

--- Sources/Border/Border.swift
+++ Sources/Border/Border.swift
@@ -17,24 +17,5 @@ extension Warp {
         public static let borderWidth25: CGFloat = 2
         /// Border width of 4 points.
         public static let borderWidth50: CGFloat = 4
-
-//        var description: CGFloat {
-//            switch self {
-//            case .borderRadius25:
-//                return 2
-//            case .borderRadius50:
-//                return 4
-//            case .borderRadius100:
-//                return 8
-//            case .borderRadius200:
-//                return 16
-//            case .borderWidth12:
-//                return 1
-//            case .borderWidth25:
-//                return 2
-//            case .borderWidth50:
-//                return 4
-//            }
-//        }
     }
 }
