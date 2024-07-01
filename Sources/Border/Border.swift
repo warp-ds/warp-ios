7e2a603c6dce0bf6266d0ebd9d9385f35a95557d
Brian Dinsen <brian.dinsen@schibsted.com>
2024-07-01 10:59:27 +0200

    Change Spacing and Border to use let instead to avoid using rawValue on enum cases

--- Sources/Border/Border.swift
+++ Sources/Border/Border.swift
@@ -1,40 +1,40 @@
 import CoreFoundation
 
 extension Warp {
-    public enum Border: CGFloat {
+    public enum Border {
         /// Border radius of 2 points.
-        case borderRadius25
+        public static let borderRadius25: CGFloat = 2
         /// Border radius of 4 points.
-        case borderRadius50
+        public static let borderRadius50: CGFloat = 4
         /// Border radius of 8 points.
-        case borderRadius100
+        public static let borderRadius100: CGFloat = 8
         /// Border radius of 16 points.
-        case borderRadius200
+        public static let borderRadius200: CGFloat = 16
 
         /// Border width of 1 points.
-        case borderWidth12
+        public static let borderWidth12: CGFloat = 1
         /// Border width of 2 points.
-        case borderWidth25
+        public static let borderWidth25: CGFloat = 2
         /// Border width of 4 points.
-        case borderWidth50
+        public static let borderWidth50: CGFloat = 4
 
-        var description: CGFloat {
-            switch self {
-            case .borderRadius25:
-                return 2
-            case .borderRadius50:
-                return 4
-            case .borderRadius100:
-                return 8
-            case .borderRadius200:
-                return 16
-            case .borderWidth12:
-                return 1
-            case .borderWidth25:
-                return 2
-            case .borderWidth50:
-                return 4
-            }
-        }
+//        var description: CGFloat {
+//            switch self {
+//            case .borderRadius25:
+//                return 2
+//            case .borderRadius50:
+//                return 4
+//            case .borderRadius100:
+//                return 8
+//            case .borderRadius200:
+//                return 16
+//            case .borderWidth12:
+//                return 1
+//            case .borderWidth25:
+//                return 2
+//            case .borderWidth50:
+//                return 4
+//            }
+//        }
     }
 }
