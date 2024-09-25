184283cb26638e8f3927044e80ffdefad7ad2c0b
Mohsen <mohsen.hosseinpour@schibsted.com>
2024-09-25 15:49:18 +0200

    Remove extra layer of logic for fonts

--- Sources/Typography/View+Font.swift
+++ Sources/Typography/View+Font.swift
@@ -1,14 +1,26 @@
 import Foundation
 import SwiftUI
 
-public extension View {
+public extension Text {
     /// Sets the font of the text in this view using the specified typography.
     ///
-    /// This method applies the font from the given `Typography` to any view, preserving the original view type.
+    /// This method applies the font from the given `Typography` to Text view.
     ///
     /// - Parameter typography: The `Typography` from which to apply the font.
     /// - Returns: The original view with the specified font applied.
-    @ViewBuilder
+    func font(from typography: Warp.Typography) -> Text {
+        self.font(typography.font)
+    }
+}
+
+
+public extension View {
+    /// Sets the font of the text in this view using the specified typography.
+    ///
+    /// This method applies the font from the given `Typography` to any view, returning some View.
+    ///
+    /// - Parameter typography: The `Typography` from which to apply the font.
+    /// - Returns: The original view with the specified font applied.
     func font(from typography: Warp.Typography) -> some View {
         self.font(typography.font)
     }
