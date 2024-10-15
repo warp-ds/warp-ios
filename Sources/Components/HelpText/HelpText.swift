5ea3c170ad69b1e57cc0db352191e52b6223bf66
Ali NaranjiSani <ali.naranji.sani@schibsted.com>
2024-10-15 10:25:58 +0200

    Add more documentation

--- Sources/Components/HelpText/HelpText.swift
+++ Sources/Components/HelpText/HelpText.swift
@@ -2,14 +2,33 @@ import SwiftUI
 
 extension Warp {
 
+    /// A view that displays help text with customizable styles.
+    ///
+    /// The `Warp.HelpText` component is used to present supplementary information, hints, or messages to the user. It supports different styles to represent various states like default, error, or disabled.
+    ///
+    /// **Usage:**
+    ///
+    /// ```swift
+    /// Warp.HelpText(text: "This is a help text.", style: .default)
+    /// ```
+    ///
+    /// - Parameters:
+    ///   - text: The text to display.
+    ///   - style: The style to apply to the text. Defaults to `.default`.
     public struct HelpText: View {
 
+        // MARK: - Properties
+
+        /// Object responsible for providing colors in different environments and variants.
         private let colorProvider: ColorProvider = Warp.Color
 
+        /// The text to display.
         private let text: String
 
+        /// The style to apply to the text.
         private let style: Warp.HelpTextStyle
 
+        // MARK: - Initialization
 
         /// Creates a new `HelpText` instance.
         ///
@@ -24,6 +43,7 @@ extension Warp {
             self.style = style
         }
 
+        // MARK: - Body
 
         public var body: some View {
             Text(
@@ -33,6 +53,7 @@ extension Warp {
             )
         }
 
+        // MARK: - Computed Properties
 
         /// Determines the text color based on the selected style.
         private var textColor: Color {
