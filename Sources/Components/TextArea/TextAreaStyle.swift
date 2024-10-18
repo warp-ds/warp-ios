6bb7a8e77fd299755992e1fce9d00e476ffce610
Ali NaranjiSani <ali.naranji.sani@schibsted.com>
2024-10-18 11:01:18 +0200

    Improvements on TextAreaStyle

--- Sources/Components/TextArea/TextAreaStyle.swift
+++ Sources/Components/TextArea/TextAreaStyle.swift
@@ -1,10 +1,7 @@
-import SwiftUI
-
 extension Warp {
-    /// Represents the different styles a TextArea can have.
+    /// Represents the different styles a `TextArea` can have.
     public enum TextAreaStyle: String, CaseIterable {
         case `default`
-        case active
         case disabled
         case error
         case readOnly
