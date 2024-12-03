e2c7a5ed49650a02cf8367a2a10326163c2b561f
Benjamin Tegelaár-Breiby <benjamin.breiby@schibsted.com>
2024-12-03 10:56:41 +0100

    Revert "Add isPressed as binding"
    This reverts commit 6d5ce5f7120473965e767a7165b7b9c1f92e377c.

--- SampleApp/Components/ShadowView.swift
+++ SampleApp/Components/ShadowView.swift
@@ -4,14 +4,13 @@ import SwiftUI
 struct ShadowView: View {
     private let colorProvider: ColorProvider = Warp.Color
     @State private var shadow = Warp.Shadow.small
-    @State private var isPressed = false
 
     var body: some View {
         ZStack {
             VStack(spacing: 0) {
                 ZStack(alignment: .bottom) {
-                    colorProvider.token.background
-                    Warp.Text("background", style: .caption)
+                    colorProvider.token.surfaceElevated100
+                    Warp.Text("surfaceElevated100", style: .caption)
                         .padding(.bottom)
                 }
                 ZStack(alignment: .top) {
@@ -23,12 +22,12 @@ struct ShadowView: View {
             .ignoresSafeArea()
             VStack {
                 VStack {
-                    Warp.Text(isPressed ? "surfaceElevated200Active" : "surfaceElevated200" , style: .caption)
+                    Warp.Text("surfaceElevated200", style: .caption)
                 }
                 .frame(width: 192, height: 192)
-                .background(isPressed ? colorProvider.token.surfaceElevated200Active : colorProvider.token.surfaceElevated200)
+                .background(colorProvider.token.surfaceElevated200)
                 .cornerRadius(8)
-                .addShadow(shadow, isPressed: $isPressed)
+                .addShadow(shadow)
                 Spacer()
                 Picker("Pick your shadow", selection: $shadow.animation(.interpolatingSpring)) {
                     ForEach(Warp.Shadow.allCases, id: \.self) { shadow in
