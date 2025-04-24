8c95bcb24814bee63a5de5cfe435591f1b137bd8
Benjamin Tegelaár-Breiby <benjamin.breiby@schibsted.com>
2025-04-24 14:31:04 +0200

    Update comments

--- Sources/Previews/ThemePreviewModifier.swift
+++ Sources/Previews/ThemePreviewModifier.swift
@@ -1,8 +1,8 @@
 import SwiftUI
 
-/// A preview modifier that sets the Warp theme.
+/// A preview modifier that sets the Warp theme in the preview.
 ///
-/// Use like #Preview(traits: .themeFinn)
+/// Usage: `#Preview(traits: .themeFinn)`
 public struct ThemePreviewModifier: PreviewModifier {
     init(theme: Warp.Brand) {
         Warp.Theme = theme
@@ -17,8 +17,15 @@ public struct ThemePreviewModifier: PreviewModifier {
 
 @available(iOS 18.0, *)
 public extension PreviewTrait where T == Preview.ViewTraits {
+    /// Preview modifier that sets the Blocket Warp theme.
     static let themeBlocket = PreviewTrait.modifier(ThemePreviewModifier(theme: .blocket))
+
+    /// Preview modifier that sets the DBA Warp theme.
     static let themeDba = PreviewTrait.modifier(ThemePreviewModifier(theme: .dba))
+
+    /// Preview modifier that sets the FINN Warp theme.
     static let themeFinn = PreviewTrait.modifier(ThemePreviewModifier(theme: .finn))
+
+    /// Preview modifier that sets the Tori Warp theme.
     static let themeTori = PreviewTrait.modifier(ThemePreviewModifier(theme: .tori))
 }
